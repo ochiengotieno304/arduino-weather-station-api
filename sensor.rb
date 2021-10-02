e8e2f6004e2af4b7e04ad2b64ca0802c4e4d9e34
Ochieng Otieno <ochiengotieno71@gmail.com>
2021-10-02 20:58:51 +0300

    update sensor.rb

--- sensor.rb
+++ sensor.rb
@@ -1,9 +1,3 @@
-#
-# This is an example of how to use the sensor class
-# You must register data callbacks and have the main thread
-# sleep or in someway keep running or your program
-# will exit before any callbacks can be called
-#
 require 'bundler/setup'
 require 'dino'
 
